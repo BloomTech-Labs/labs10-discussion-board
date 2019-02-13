fadadbe3bc00b087ba4d3730e3da4a31cacabdb3
Carlos Vargas <cornielleandres@gmail.com>
2019-02-13 14:31:44 -0500

    Modify seeds for discussions table.

--- backend/db/seeds/003-discussions.js
+++ backend/db/seeds/003-discussions.js
@@ -6,28 +6,19 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
-const titleArr =[
-  'React Hooks',
-  'New in Python',
-  'One Piece Theories',
-  'Kobe Bryant: GOAT or NO?',
-  'Simpsons: How Long Will They Go?',
-]
-
 const generateSeeds = () => {
   let arr = []
   for (let i=0; i < 5; i++){
     arr.push({
         user_id: i+1,
         category_id: getRandomIntInclusive(1,7),
-        title: titleArr[i],
-        body: faker.lorem.sentence(),
+        title: faker.lorem.sentence(4),
+        body: faker.lorem.sentences(3),
         created_at: faker.date.recent(3)
     })
   }
   return arr
 }
-
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
