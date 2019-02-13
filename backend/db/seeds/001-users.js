5246f841ef54e966441d6c666cdfdc677aa7b85b
Huthman King <kinghuthman@gmail.com>
2019-02-12 21:46:19 -0500

    User Routes/Mock Data installed faker as a dev dependency updated db with 500 mock users able to get users accessing /users/ api

--- backend/db/seeds/001-users.js
+++ backend/db/seeds/001-users.js
@@ -1,13 +1,28 @@
+
+// External API containing mock data for various endpoints
+const faker = require('faker')
+
+// For loop to generate 500 users
+const generateSeeds = () => {
+  let arr = []
+  for (let i=0; i < 500; i++){
+    arr.push({
+        username: faker.internet.userName(),
+        password: 'pass',
+        email: faker.internet.email(),
+        status: 'active'
+    })
+  }
+  return arr
+}
+
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users')
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex('users').insert([
-        { id: 1, colName: 'rowValue1' },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' }
-      ]);
+      return knex('users').insert(generateSeeds());
     });
 };
