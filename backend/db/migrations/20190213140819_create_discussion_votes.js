057a7181c6edc1cbece51aeb47f72bf0b4a953c9
Carlos Vargas <cornielleandres@gmail.com>
2019-02-13 14:31:30 -0500

    Add migration to create discussion_votes table.

---
+++ backend/db/migrations/20190213140819_create_discussion_votes.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('discussion_votes', function(tbl) {
+		//Foreign Key 'discussion_id'
+		tbl
+			.integer('discussion_id')
+			.references('id')
+			.inTable('discussions')
+			.notNullable();
+
+		//Foreign Key 'user_id'
+		tbl
+			.integer('user_id')
+			.references('id')
+			.inTable('users')
+			.notNullable();
+
+		// 1 for upvote / -1 for downvote
+		tbl
+			.integer('type')
+			.notNullable();
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('discussion_votes');
+};
