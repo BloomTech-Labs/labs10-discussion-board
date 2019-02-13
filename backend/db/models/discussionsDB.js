9b52927141d74e628d23c54847987263da049c10
Carlos Vargas <cornielleandres@gmail.com>
2019-02-13 14:31:16 -0500

    Add discussionsDB.

---
+++ backend/db/models/discussionsDB.js
@@ -0,0 +1,29 @@
+const db = require('../dbConfig.js');
+
+// get top (limit 10) daily discussions ordered by vote_count
+const getTopDailyDiscussions = () => {
+    return db('discussions as d')
+        .select(
+            'd.id',
+			'd.user_id',
+			'u.username',
+			'd.category_id',
+			'c.name as category_name',
+            'd.title',
+            'd.body',
+            'd.created_at',
+        )
+        .sum('dv.type as vote_count')
+        .join('discussion_votes as dv', 'dv.discussion_id', 'd.id')
+        .join('users as u', 'u.id', 'd.user_id')
+        .join('categories as c', 'c.id', 'd.category_id')
+        // this whereRaw gets the created_at dates that are 24 hours away from the current time
+        .whereRaw("created_at >= ?", [new Date(new Date().getTime() - (24 * 60 * 60 * 1000))])
+        .limit(10)
+        .groupBy('d.id', 'u.username', 'd.title', 'c.name', 'd.category_id')
+        .orderBy('vote_count', 'desc');
+};
+
+module.exports = {
+    getTopDailyDiscussions,
+};
--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,7 +1,9 @@
+const discussionsDB = require('./discussionsDB.js');
 const postsDB = require('./postsDB.js');
 const usersDB = require('./usersDB.js');
 
 module.exports = {
+	discussionsDB,
 	postsDB,
 	usersDB,
 };
