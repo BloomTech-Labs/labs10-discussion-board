8c9007179150ad532df252eea1c7ddc97f690eb5
David Situ <davidsitu626@gmail.com>
2019-02-13 10:32:59 -0800

    updated: added body to discussions table

--- backend/db/seeds/003-discussions.js
+++ backend/db/seeds/003-discussions.js
@@ -5,11 +5,16 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('discussions').insert([
-        {user_id: 1, category_id: 1, title: 'React Hooks'},
-        {user_id: 4, category_id: 1, title: 'New in Python'},
-        {user_id: 5, category_id: 4, title: 'One Piece Theories'},
-        {user_id: 3, category_id: 2, title: 'Kobe Bryant: GOAT or NO?'},
-        {user_id: 10, category_id: 5, title: 'Simpsons: How Long Will They Go?'},
+        {user_id: 1, category_id: 1, title: 'React Hooks', 
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '},
+        {user_id: 4, category_id: 1, title: 'New in Python', 
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ' },
+        {user_id: 5, category_id: 4, title: 'One Piece Theories', 
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '},
+        {user_id: 3, category_id: 2, title: 'Kobe Bryant: GOAT or NO?', 
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '},
+        {user_id: 10, category_id: 5, title: 'Simpsons: How Long Will They Go?', 
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '}
       ]);
     });
 };
