c49505f65b7b046b1c62e1ff1d291d43dcbb99c3
Carlos Vargas <cornielleandres@gmail.com>
2019-02-12 20:00:13 -0500

    Add LandingView as route.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
-import LandingView from './views/LandingView.js';
 
-const DivWrapper = styled.div`
-  display: flex;
+// components
+import { LandingView } from './views/index.js';
+
+const AppWrapper = styled.div`
+	display: flex;
 `;
 
 const GlobalStyle = createGlobalStyle`
-  html,
-  body,
-  #root {
-    margin: 0;
-    padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-  }
+	html,
+	body,
+	#root {
+    	margin: 0;
+    	padding: 0;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+		'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+		sans-serif;
+	}
 `;
 
 class App extends Component {
-  render() {
-    return (
-      <DivWrapper>
-        <GlobalStyle />
-        <LandingView />
-        hello world
-      </DivWrapper>
-    );
-  }
+	render() {
+		return (
+			<AppWrapper>
+				<GlobalStyle />
+				<Route exact path = '/' component = { LandingView} />
+			</AppWrapper>
+		);
+	}
 }
 
 export default App;
