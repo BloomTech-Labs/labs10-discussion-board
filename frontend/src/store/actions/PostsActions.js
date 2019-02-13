735831c50cbb27ff8a7ef61d72c4f9b70a1b4af7
Carlos Vargas <cornielleandres@gmail.com>
2019-02-13 13:01:09 -0500

    Add actions and action creator for getting top posts.

--- frontend/src/store/actions/PostsActions.js
+++ frontend/src/store/actions/PostsActions.js
@@ -2,4 +2,19 @@ import axios	from 'axios';
 
 const backendURL = process.env.REACT_APP_BACKEND_URL;
 
-export const getTopPosts = () => dispatch => {};
+/***************************************************************************************************
+ ********************************************* Actions *******************************************
+ **************************************************************************************************/
+export const TOP_POSTS_LOADING = 'TOP_POSTS_LOADING';
+export const TOP_POSTS_SUCCESS = 'TOP_POSTS_SUCCESS';
+export const TOP_POSTS_FAILURE = 'TOP_POSTS_FAILURE';
+
+/***************************************************************************************************
+ ********************************************** Actions ********************************************
+ **************************************************************************************************/
+export const getTopPosts = () => dispatch => {
+	dispatch({ type: TOP_POSTS_LOADING });
+	return axios.get(`${ backendURL }/posts/top-daily`)
+		.then(res => dispatch({ type: TOP_POSTS_SUCCESS, payload: res.data }))
+		.catch(err => console.log(err));
+};
