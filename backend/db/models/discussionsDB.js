a23da0c0bf9231a406d6573b1a30f3a555aa98ff
David Situ <davidsitu626@gmail.com>
2019-02-13 14:34:40 -0800

    Update: discussions.js added get, find, add, insert, delete functions

--- backend/db/models/discussionsDB.js
+++ backend/db/models/discussionsDB.js
@@ -24,6 +24,56 @@ const getTopDailyDiscussions = () => {
         .orderBy('vote_count', 'desc');
 };
 
+//gets All Discussions
+const getDiscussions = () => {
+    return db('discussions')
+};
+
+//Find By ID (discussions own ID)
+const findById = (id) => {
+    return db('discussions').where('id', id)
+};
+
+//Find by User ID (Original Creator)
+const findByUserId = (user_id) => {
+    return db('discussions').where('user_id', user_id)
+};
+
+//Find by Associated Category (category ID)
+const findByCategoryId = (category_id) => {
+    return db('discussions').where('category_id', category_id)
+};
+
+
+//AUTHORIZED ACCESS
+
+//Add Discussion into the Discussion table
+const insert = (discussion) => {
+    return db('discussions').insert(discussion)
+};
+
+
+//EDIT [ACCOUNT TYPE ACCESS: USER_ID]
+const update = (discussion, id) => {
+    return db('discussions')
+            .where('id', id)
+            .update(discussion)
+};
+
+//DELETE [ACCOUNT TYPE ACCESS: USER_ID, ADMIN]
+const remove = (id) => {
+    return db('discussions')
+            .where('id', id)
+            .del()
+}
+
 module.exports = {
     getTopDailyDiscussions,
+    getDiscussions,
+    findById,
+    findByUserId,
+    findByCategoryId,
+    insert,
+    update,
+    remove
 };
