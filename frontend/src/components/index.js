9a85f8191985450ecd53eeab469f65b062a0d589
Carlos Vargas <cornielleandres@gmail.com>
2019-02-14 12:23:25 -0500

    Add PostCount and VoteCount components.

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,5 +4,7 @@ import TopPost from './TopPost.js';
 import Header from './Header.js';
 import TopDiscussions from './TopDiscussions.js';
 import TopDiscussion from './TopDiscussion.js';
+import PostCount from './PostCount.js';
+import VoteCount from './VoteCount.js';
 
-export { LoginDropdown, Nav, TopPost, Header, TopDiscussions, TopDiscussion };
+export { LoginDropdown, Nav, TopPost, Header, TopDiscussions, TopDiscussion, PostCount, VoteCount };
