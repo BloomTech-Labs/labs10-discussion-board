1170c62c2290a17bf6281e96132056dad8e65644
Lucas Beemer <lucasbeemer@gmail.com>
2019-02-15 13:15:41 -0800

    deleted import that wasn't being used

--- frontend/src/views/TopDiscussionsView.js
+++ frontend/src/views/TopDiscussionsView.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 import Discuss from '../assets/img/Discuss.png';
 import TextLoop from "react-text-loop";
