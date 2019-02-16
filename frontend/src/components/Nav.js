f69b7fcf89c46ff0c8ffa4872114fd8aea65654e
Carlos Vargas <cornielleandres@gmail.com>
2019-02-16 11:52:40 -0500

    Pass username as prop, add welcome message, and update signout function.

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
@@ -36,6 +36,11 @@ const Login = styled.a`
   }
 `;
 
+const Welcome = styled.div`
+  margin: 25px;
+  font-size: 24px;
+`;
+
 const Signout = styled.a`
   font-size: 30px;
   user-select: none;
@@ -69,20 +74,27 @@ class Nav extends Component {
 
   clickSignout = ev => {
     ev.preventDefault();
-    this.setState({ isLoginClicked: false }, () => this.props.signout());
+    this.setState({ isLoginClicked: false }, () => {
+      localStorage.removeItem('symposium_auth0_access_token');
+      localStorage.removeItem('symposium_auth0_expires_at');
+      return this.props.signout().then(() => this.props.history.push('/'));
+    });
   };
 
   render() {
     return (
       <DivWrapper>
         {this.props.isLoggedIn ? (
-          <Signout
-            onClick={ev => {
-              this.clickSignout(ev);
-            }}
-          >
-            Sign Out
-          </Signout>
+          <Fragment>
+            <Welcome>Welcome, { this.props.username }!</Welcome>
+            <Signout
+              onClick={ev => {
+                this.clickSignout(ev);
+              }}
+            >
+              Sign Out
+            </Signout>
+          </Fragment>
         ) : (
           <Auth>
             <Register>Register</Register> |{' '}
@@ -109,6 +121,7 @@ class Nav extends Component {
 
 const mapStateToProps = state => {
   return {
+    username: state.users.username,
     isLoggedIn: state.users.isLoggedIn,
     loggingInLoadingMessage: state.users.loggingInLoadingMessage
   };
