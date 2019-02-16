326ca674474442c580072c5cab002a3c1c184ebb
Huthman King <kinghuthman@gmail.com>
2019-02-16 00:33:53 -0500

    duplicate background color in app.js removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ const AppWrapper = styled.div`
   flex-direction: column;
   min-height: 100vh;
   width: 95vw;
-  background: #EEE7C8;
+  
 `;
 
 const GlobalStyle = createGlobalStyle`
@@ -27,12 +27,12 @@ const GlobalStyle = createGlobalStyle`
 	#root {
     	margin: 0;
     	padding: 0;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-		display: flex;
-		align-items: center;
-		flex-wrap: wrap;
-    flex-direction: column;
-    background: #EEE7C8;
+		  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+		  display: flex;
+		  align-items: center;
+		  flex-wrap: wrap;
+      flex-direction: column;
+      background: #EEE7C8;
 	}
 `;
 
