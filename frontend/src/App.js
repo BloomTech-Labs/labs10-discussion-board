d841998031da4c9ab3f9ebe38665c17502e636eb
Carlos Vargas <cornielleandres@gmail.com>
2019-02-16 11:45:09 -0500

    Add auth0 functionality to App component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import auth0 from 'auth0-js';
+import { Auth0Lock } from 'auth0-lock';
+import { connect } from 'react-redux';
 import styled, { createGlobalStyle } from 'styled-components';
 
 // components
@@ -7,6 +10,16 @@ import { Header, Profiles } from './components/index.js';
 
 // views
 import { LandingView, CategoriesView } from './views/index.js';
+
+// action creators
+import { auth0Login } from './store/actions/index.js';
+
+// globals
+import {
+  auth0Domain,
+  auth0ClientID,
+  auth0RedirectUri,
+} from './globals/globals.js';
 
 const AppWrapper = styled.div`
   display: flex;
@@ -32,18 +45,73 @@ const GlobalStyle = createGlobalStyle`
 	}
 `;
 
+const lock = new Auth0Lock(
+  auth0ClientID,
+  auth0Domain,
+);
+
+const webAuth = new auth0.WebAuth({
+  domain: auth0Domain,
+  clientID: auth0ClientID,
+  redirectUri: auth0RedirectUri,
+});
+
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    webAuth.parseHash((err, authResult) => {
+      if (authResult) {
+        const { accessToken, expiresIn } = authResult;
+        const expiresAt = JSON.stringify(expiresIn * 1000 + new Date().getTime());
+        localStorage.setItem('symposium_auth0_access_token', accessToken);
+        localStorage.setItem('symposium_auth0_expires_at', expiresAt);
+        return this.props.auth0Login(accessToken);
+      } else if (err) console.log(err);
+    });
+  };
+  handleLogin = () => {
+    lock.show();
+  };
+  handleLogout() {
+    // Clear Access Token and ID Token from local storage
+    localStorage.removeItem('symposium_auth0_access_token');
+    localStorage.removeItem('symposium_auth0_expires_at');
+    window.location.reload();
+  };
+  isAuthenticated() {
+    // check whether the current time is past the access token's expiry time
+    const expiresAt = localStorage.getItem('symposium_auth0_expires_at');
+    return new Date().getTime() < expiresAt;
+  };
+
   render() {
-    return (
-      <AppWrapper>
-        <GlobalStyle />
-        <Header />
-        <Route exact path='/home' component={LandingView} />
-        <Route exact path='/profiles' component={Profiles} />
-        <Route path='/categories' component={CategoriesView} />
-      </AppWrapper>
-    );
+    if (this.isAuthenticated()) {
+      return (
+        <AppWrapper>
+          <GlobalStyle />
+          <Header />
+  
+          <button onClick = { this.handleLogout }>Logout</button>
+  
+          <Route exact path='/home' component={LandingView} />
+          <Route exact path='/profiles' component={Profiles} />
+          <Route path='/categories' component={CategoriesView} />
+        </AppWrapper>
+      );
+    } else {
+      return (
+        <div>
+          <p>
+            You are not authorized to view this content, please click below to
+            LOGIN.
+          </p>
+          <button onClick = { this.handleLogin }>Login</button>
+        </div>
+      );
+    }
   }
 }
 
-export default App;
+export default connect(null, { auth0Login })(App);
