bcb6ed924692508130e6a66c520eb53c4c78c47b
Carlos Vargas <cornielleandres@gmail.com>
2019-02-16 11:54:33 -0500

    Handle auth0 login actions.

--- frontend/src/store/reducers/UsersReducer.js
+++ frontend/src/store/reducers/UsersReducer.js
@@ -2,11 +2,17 @@ import {
   USER_LOGIN_LOADING,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAILURE,
-  USER_SIGNOUT_SUCCESS
+
+  USER_SIGNOUT_SUCCESS,
+
+  USER_AUTH0_LOGIN_LOADING,
+  USER_AUTH0_LOGIN_SUCCESS,
+  USER_AUTH0_LOGIN_FAILURE,
 } from '../actions/index.js';
 
 const initialState = {
   user_id: 0,
+  username: '',
   isLoggedIn: false,
   loggingInLoadingMessage: false,
   loggingOutLoadingMessage: false,
@@ -17,30 +23,39 @@ const initialState = {
 export const UsersReducer = (state = initialState, action) => {
   switch (action.type) {
     // Login
+    case USER_AUTH0_LOGIN_LOADING:
     case USER_LOGIN_LOADING:
       return {
         ...state,
         loggingInLoadingMessage: true,
         error: null
       };
+    case USER_AUTH0_LOGIN_SUCCESS:
     case USER_LOGIN_SUCCESS:
       return {
         ...state,
         user_id: action.payload.id,
+        username: action.payload.username,
         isLoggedIn: true,
         loggingInLoadingMessage: false,
         token: action.payload.token,
         error: null
       };
+    case USER_AUTH0_LOGIN_FAILURE:
     case USER_LOGIN_FAILURE:
       return {
         ...state,
         loggingInLoadingMessage: false,
         error: action.payload
       };
+
     // Signout
     case USER_SIGNOUT_SUCCESS:
       return initialState;
+
+    case USER_AUTH0_LOGIN_LOADING:
+    case USER_AUTH0_LOGIN_SUCCESS:
+    case USER_AUTH0_LOGIN_FAILURE:
     default:
       return state;
   }
