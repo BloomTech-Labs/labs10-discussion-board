b45e85ada175133b267c3ada3cc1896eccd24051
jamespagedev <jamespagedev@outlook.com>
2019-02-15 22:22:52 -0700

    fill background color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,20 +15,20 @@ const AppWrapper = styled.div`
   flex-direction: column;
   min-height: 100vh;
   width: 95vw;
-  background: gray;
 `;
 
 const GlobalStyle = createGlobalStyle`
 	html,
 	body,
 	#root {
-    	margin: 0;
-    	padding: 0;
+    margin: 0;
+    padding: 0;
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
 		display: flex;
 		align-items: center;
 		flex-wrap: wrap;
 		flex-direction: column;
+    background: gray;
 	}
 `;
 
