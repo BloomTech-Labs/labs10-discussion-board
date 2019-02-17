f7f84276a5fb0d066c3798f08431591730ac4f6a
Carlos Vargas <cornielleandres@gmail.com>
2019-02-16 23:11:00 -0500

    Update endpoint for creating posts.

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -18,9 +18,11 @@ const { authenticate } = require('../config/middleware/authenticate.js');
 // create a post by a given user_id to a given discussion_id
 router.post('/:user_id', authenticate, async (req, res, next) => {
 	try {
-		const { discussion_id, body } = req.body;
+		const { user_id } = req.params;
+		const { discussion_id, postBody } = req.body;
+		if (!postBody) return res.status(400).json({ error: 'Post body must not be empty.' });
 		return postsDB
-			.insert(user_id, discussion_id, body)
+			.insert(user_id, discussion_id, postBody)
 			.then(() => res.status(201).json({ message: 'Post creation successful.' }));
 	} catch (err) {
 		next(err);
