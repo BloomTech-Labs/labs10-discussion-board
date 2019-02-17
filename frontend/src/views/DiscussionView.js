b731f82ce4cf9150f70ff701148a455950763d18
Carlos Vargas <cornielleandres@gmail.com>
2019-02-16 21:45:24 -0500

    Update DiscussionView. Convert to functional component.

--- frontend/src/views/DiscussionView.js
+++ frontend/src/views/DiscussionView.js
@@ -1,51 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import moment from 'moment';
+import React from 'react';
 import styled from 'styled-components';
 
-// views
-import { PostsView } from './index.js';
-
-// action creators
-import { getDiscussionById } from '../store/actions/index.js';
+// components
+import { Discussion } from '../components/index.js';
 
 const DiscussionViewWrapper = styled.div`
     width: 90%;
-`
+`;
 
-class DiscussionView extends Component {
-	componentDidMount = () => this.props.getDiscussionById(this.props.match.params.id);
-	render() {
-		const { discussion } = this.props;
-		const {
-			body,
-			category_id,
-			category_name,
-			created_at,
-			discussion_votes,
-			id,
-			posts,
-			title,
-			user_id,
-			username,
-		} = discussion;
-		return(
-			<DiscussionViewWrapper>
-				<h1>DISCUSSION</h1>
-				<p>/d/{ category_name }</p>
-				<p>Discussion Votes: { discussion_votes }</p>
-				<p>Posted by: { username } { moment(created_at).fromNow() }</p>
-				<p>Title: { title }</p>
-				<p>Body: { body }</p>
-
-				<PostsView posts = { posts } />
-			</DiscussionViewWrapper>
-		)
-	}
+const DiscussionView = ({ match }) => {
+	const { id } = match.params;
+	return(
+		<DiscussionViewWrapper>
+			<Discussion id = { id } />
+		</DiscussionViewWrapper>
+	);
 };
 
-const mapStateToProps = state => ({
-	discussion: state.discussions.discussion,
-});
-
-export default connect(mapStateToProps, { getDiscussionById })(DiscussionView);
+export default DiscussionView;
