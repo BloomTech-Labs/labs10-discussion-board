237869378a5dd754c8fa80eb034b7375947f70e6
jamespagedev <jamespagedev@outlook.com>
2019-02-18 20:48:33 -0700

    fix avatar size bug and ready for development merge

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -2,21 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const AvatarWrapper = styled.div`
-	display: inline-block;
-	height: ${ ({ height }) => height };
-	width: ${ ({ width }) => width };
-	background-image: ${ ({ src }) => `url('${ src }')` };
-	background-size: contain;
-	background-repeat: no-repeat;
-	background-position: center;
+  display: inline-block;
+  min-height: ${({ height }) => height};
+  min-width: ${({ width }) => width};
+  background-image: ${({ src }) => `url('${src}')`};
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
 `;
 
 const Avatar = ({ height, width, src }) => {
-	return <AvatarWrapper
-		height = { height }
-		width = { width }
-		src = { src }
-	/>;
+  return <AvatarWrapper height={height} width={width} src={src} />;
 };
 
 export default Avatar;
