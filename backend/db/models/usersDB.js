db892821438dc06f48df2a4710b1375849eedca0
Huthman King <kinghuthman@gmail.com>
2019-02-19 11:30:38 -0500

    BE - User model to follow a given discussion created

--- backend/db/models/usersDB.js
+++ backend/db/models/usersDB.js
@@ -22,6 +22,10 @@ const findById = id => {
       return getUserResults;
     });
 };
+// User follows a discusson
+const followDiscussion = (discussion_id, title) => {
+  return db('users').insert({discussion_id, title});
+}
 
 // gets password for user with given id
 const getPassword = id => {
@@ -93,4 +97,5 @@ module.exports = {
   update,
   updatePassword,
   remove,
+  followDiscussion,
 };
