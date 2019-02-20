af050fa9a0e8e1fca313ac7e51250b54b015100a
Huthman King <kinghuthman@gmail.com>
2019-02-20 13:47:04 -0500

    FE - Follow a category Actions added to index

--- frontend/src/store/actions/index.js
+++ frontend/src/store/actions/index.js
@@ -1,10 +1,18 @@
 import {
   // Actions
+  //GET CATEGORIES
   GET_CATEGORIES_LOADING,
   GET_CATEGORIES_SUCCESS,
   GET_CATEGORIES_FAILURE,
+
+  //FOLLOW CATEGORY
+  FOLLOW_CATEGORY_LOADING, 
+  FOLLOW_CATEGORY_SUCCESS,
+  FOLLOW_CATEGORY_FAILURE, 
+
   // Action Creators
-  getCategories
+  getCategories,
+  followCategory,
 } from './CategoriesActions.js';
 
 import {
@@ -133,7 +141,11 @@ export {
 	//Categories Actions
 	GET_CATEGORIES_LOADING,
 	GET_CATEGORIES_SUCCESS,
-	GET_CATEGORIES_FAILURE,
+  GET_CATEGORIES_FAILURE,
+  
+  FOLLOW_CATEGORY_LOADING, 
+  FOLLOW_CATEGORY_SUCCESS,
+  FOLLOW_CATEGORY_FAILURE,
 
 	// Discussion Actions
 	GET_DISCUSSIONS_LOADING,
@@ -221,7 +233,8 @@ export {
   EMAIL_EXISTS_FAILURE,
 
 	// Categories Action Creators
-	getCategories,
+  getCategories,
+  followCategory,
 
 	// Discussion Action Creators
 	getTopDiscussions,
