5a70d2a025134faf76c193e3f65649a182556ccf
David Situ <davidsitu626@gmail.com>
2019-02-20 13:42:37 -0800

    Updated postVoteRouter.js: if user already voted

--- backend/routes/postVoteRouter.js
+++ backend/routes/postVoteRouter.js
@@ -24,6 +24,21 @@ router.post('/', (req, res) => {
     ){
         return res.status(400).json({ error:  'post_id, user_id, and type must all be integers.' })
     }
+        //Check to see if User has already voted
+        return postVotesDB.get(post_id, user_id)
+        .then(post => {
+            //If user had already voted
+            if (post.length) {
+                //and it was the same vote type
+            } if(post[0].type === type) {
+                //then remove the vote
+                return postVotesDB.remove(post.id, user.id)
+                    .then(() => res.status(201).json({ error: 'Vote has been removed' }))
+                    .catch(() => res.status(500).json({ error: `Failed to update(): ${err}` }))
+                    
+            }
+            //Else user has not already voted
+        })
 })
 
 module.exports = router;
