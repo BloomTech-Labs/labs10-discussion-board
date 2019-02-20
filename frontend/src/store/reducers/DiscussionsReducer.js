2e182e1c3fc2173aae55b13698c96dae19bcb9a7
David Situ <davidsitu626@gmail.com>
2019-02-20 12:16:33 -0800

    conflicting merges

--- frontend/src/store/reducers/DiscussionsReducer.js
+++ frontend/src/store/reducers/DiscussionsReducer.js
@@ -2,12 +2,15 @@ import {
   TOP_DISCUSSIONS_LOADING,
   TOP_DISCUSSIONS_SUCCESS,
   TOP_DISCUSSIONS_FAILURE,
+
   GET_DISCUSSIONS_LOADING,
   GET_DISCUSSIONS_SUCCESS,
   GET_DISCUSSIONS_FAILURE,
+
   GET_DISCUSSION_BY_ID_LOADING,
   GET_DISCUSSION_BY_ID_SUCCESS,
   GET_DISCUSSION_BY_ID_FAILURE,
+
   FOLLOW_DISCUSSION_LOADING,
   FOLLOW_DISCUSSION_FAILURE
 } from '../actions/index.js';
@@ -25,11 +28,6 @@ const initialState = {
 
 export const DiscussionsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case TOP_DISCUSSIONS_SUCCESS:
-      return {
-        ...state,
-        topDiscussions: action.payload
-      };
 
     case GET_DISCUSSION_BY_ID_SUCCESS:
       return {
@@ -37,26 +35,30 @@ export const DiscussionsReducer = (state = initialState, action) => {
         discussion: action.payload
       };
 
-    case GET_DISCUSSION_BY_ID_LOADING:
-    case GET_DISCUSSION_BY_ID_FAILURE:
-    case TOP_DISCUSSIONS_LOADING:
-    case TOP_DISCUSSIONS_FAILURE:
-      return state;
+      case TOP_DISCUSSIONS_SUCCESS:
+      return {
+        ...state,
+        topDiscussions: action.payload
+      };
 
-    case GET_DISCUSSION_BY_ID_LOADING:
-    case GET_DISCUSSION_BY_ID_FAILURE:
-    case TOP_DISCUSSIONS_LOADING:
-    case TOP_DISCUSSIONS_FAILURE:
-    case FOLLOW_DISCUSSION_LOADING:
-    case FOLLOW_DISCUSSION_FAILURE:
-    case GET_DISCUSSIONS_SUCCESS:
+      case GET_DISCUSSIONS_SUCCESS:
       return {
         ...state,
         discussionsByCat: action.payload
       };
 
+    case GET_DISCUSSION_BY_ID_LOADING:
+    case GET_DISCUSSION_BY_ID_FAILURE:
+
+    case TOP_DISCUSSIONS_LOADING:
+    case TOP_DISCUSSIONS_FAILURE:
+
+    case FOLLOW_DISCUSSION_LOADING:
+    case FOLLOW_DISCUSSION_FAILURE: 
+
     case GET_DISCUSSIONS_LOADING:
     case GET_DISCUSSIONS_FAILURE:
+
     default:
       return state;
   }
