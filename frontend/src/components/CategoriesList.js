ede097675cea1c6177d588fdcc7bfa59b9eeface
Carlos Vargas <cornielleandres@gmail.com>
2019-02-22 21:54:39 -0500

    Add ability to sort categories list by name, discussions, and date.

--- frontend/src/components/CategoriesList.js
+++ frontend/src/components/CategoriesList.js
@@ -12,11 +12,33 @@ const CategoryWrapper = styled.div`
 `;
 
 class CategoriesList extends Component {
-    componentDidMount = () => this.props.getCategories();
+	state = {
+		order: '', // possible values: 'name', 'discussion_count', 'created_at'
+		orderType: '', // possible values: 'asc', 'desc'
+    };
+    handleSelectChange = e => this.setState({ [e.target.name]: e.target.value }, () => {
+		return this.props.getCategories(this.state.order, this.state.orderType);
+	});
+    componentDidMount = () => this.props.getCategories(this.state.order, this.state.orderType);
     render() {
         const { categories } = this.props;
+        const { order } = this.state;
         return(
             <CategoryWrapper>
+                <span>Sort by: </span>
+				<select onChange = { this.handleSelectChange } name = 'order'>
+					<option value = 'name'>Name</option>
+					<option value = 'discussion_count'>Discussions</option>
+					<option value = 'created_at'>Date</option>
+				</select>
+				<select onChange = { this.handleSelectChange } name = 'orderType'>
+                    <option value = 'asc'>
+						{ order === 'created_at' ? 'Least Recent First' : 'Least First' }
+					</option>
+                    <option value = 'desc'>
+						{ order === 'created_at' ? 'Most Recent First' : 'Greatest First' }
+					</option>
+				</select>
                 {
                     categories.map((category, index) => 
                     <Categories 
