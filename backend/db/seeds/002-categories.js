5a0eb50e59a3932bb4d77e754c3f85f4b59d251a
David Situ <davidsitu626@gmail.com>
2019-02-25 16:44:13 -0800

    update changes

--- backend/db/seeds/002-categories.js
+++ backend/db/seeds/002-categories.js
@@ -27,13 +27,13 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('categories').insert([
-        {user_id: '1', name: 'Tech Talk', created_at: Date.parse(new Date(Date.now()))},
-        {user_id: '2', name: 'Sports', created_at: Date.parse(new Date(Date.now()))},
-        {user_id: '3', name: 'Cars', created_at: Date.parse(new Date(Date.now()))},
-        {user_id: '4', name: 'Anime', created_at: Date.parse(new Date(Date.now()))},
-        {user_id: '5', name: 'TV Shows', created_at: Date.parse(new Date(Date.now()))},
-        {user_id: '6', name: 'Movies', created_at: Date.parse(new Date(Date.now()))},
-        {user_id: '7', name: 'Music', created_at: Date.parse(new Date(Date.now()))},
+        {user_id: '1', name: 'Tech Talk', created_at: '1550030535456'},
+        {user_id: '2', name: 'Sports', created_at: '1550120835456'},
+        {user_id: '3', name: 'Cars', created_at: '1550220835456'},
+        {user_id: '4', name: 'Anime', created_at: '1550310735456'},
+        {user_id: '5', name: 'TV Shows', created_at: '1550430865456'},
+        {user_id: '6', name: 'Movies', created_at: '1550530895456'},
+        {user_id: '7', name: 'Music', created_at: '1550630875456'},
       ]);
     });
 };
