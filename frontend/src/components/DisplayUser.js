b32871cdded7a91c1fa82a1b31cea79df2920163
Carlos Vargas <cornielleandres@gmail.com>
2019-02-27 15:54:39 -0500

    Add ability to get and add notifications on the front-end.

--- frontend/src/components/DisplayUser.js
+++ frontend/src/components/DisplayUser.js
@@ -4,10 +4,10 @@ import { connect } from 'react-redux';
 import chevron from '../assets/img/chevron.png';
 
 // components
-import { Avatar, AvatarDropdown } from './index.js';
+import { Avatar, AvatarDropdown, Notifications } from './index.js';
 
 // action creators
-import { signout } from '../store/actions';
+import { signout, markNotificationsAsRead } from '../store/actions';
 
 /***************************************************************************************************
  ********************************************** Styles *********************************************
@@ -18,6 +18,7 @@ const DivWrapper = styled.div`
   justify-content: flex-end;
   align-items: center;
   width: 100%;
+  position: relative;
 
   @media (max-width: 750px){
       width: 100%;
@@ -56,6 +57,15 @@ const DivAvatar = styled.div`
 const PWelcomeMessage = styled.p`
     font-size: 20px;
     margin-right: 10px;
+
+    .notifications-icon {
+      ${ ({ newNotifications }) => newNotifications && 'color: red;' }
+      &:hover {
+        color: #ddd;
+        cursor: pointer;
+      }
+    }
+
     @media (max-width: 750px){
       font-size: 16px;
       width: 40%;
@@ -73,7 +83,8 @@ class DisplayUser extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isAvatarClicked: false
+      isAvatarClicked: false,
+      showNotifications: false,
     }
   }
 
@@ -85,32 +96,60 @@ class DisplayUser extends Component {
     this.setState({ isAvatarClicked: false });
   }
 
-  toggleAvatarClicked = () => {
-    this.setState({ isAvatarClicked: !this.state.isAvatarClicked });
-  }
+  toggleAvatarClicked = () => this.setState({
+    isAvatarClicked: !this.state.isAvatarClicked,
+    showNotifications: false,
+  });
+
+  toggleShowNotifications = () => this.setState({
+    showNotifications: !this.state.showNotifications,
+    isAvatarClicked: false,
+  }, () => this.props.newNotifications && this.props.markNotificationsAsRead());
 
   clickSignout = ev => {
     ev.preventDefault();
     localStorage.removeItem('symposium_auth0_access_token');
     localStorage.removeItem('symposium_auth0_expires_at');
-    return this.props.signout();
+    return this.props.signout(this.props.uuid);
   };
 
   goToProfilePage = () => this.props.history.push(`/profile/${this.props.user_id}`);
 
+  goTo = url => this.setState({ showNotifications: !this.state.showNotifications }, () => {
+    return this.props.history.push(url);
+  });
+
   render() {
+    const { isAvatarClicked, showNotifications } = this.state;
     return (
       <DivWrapper>
-        <PWelcomeMessage>Welcome, {this.props.username}</PWelcomeMessage>
+        <PWelcomeMessage newNotifications = { this.props.newNotifications }>
+          Welcome, {this.props.username}&nbsp;
+          <span className = 'notifications-icon-wrapper'>
+            { this.props.newNotifications ? this.props.newNotificationCount : null }
+            <i
+              onClick = { this.toggleShowNotifications }
+              className = 'far fa-envelope notifications-icon'
+            />
+          </span>
+        </PWelcomeMessage>
+        {
+          showNotifications &&
+          <Notifications
+            toggleShowNotifications = { this.toggleShowNotifications }
+            notifications = { this.props.notifications }
+            goTo = { this.goTo }
+          />
+        }
         <DivUser>
           <DivAvatar
             onClick={() => this.toggleAvatarClicked()}
-            isAvatarClicked={this.state.isAvatarClicked}
+            isAvatarClicked={isAvatarClicked}
           >
             <Avatar height={'72px'} width={'72px'} src={this.props.avatar} />
             <img src={chevron} alt='chevron' />
           </DivAvatar>
-          {(this.state.isAvatarClicked) && <AvatarDropdown clickSignout={this.clickSignout} user_id={this.props.user_id} />}
+          {(isAvatarClicked) && <AvatarDropdown clickSignout={this.clickSignout} user_id={this.props.user_id} />}
         </DivUser>
       </DivWrapper>
     );
@@ -122,10 +161,15 @@ const mapStateToProps = state => ({
   user_id: state.users.user_id,
   avatar: state.users.avatar,
   username: state.users.username,
-  loggingInLoadingMessage: state.users.loggingInLoadingMessage
+  uuid: state.users.uuid,
+  notifications: state.users.notifications,
+  newNotifications: state.users.newNotifications,
+  newNotificationCount: state.users.newNotificationCount,
+  last_login: state.users.last_login,
+  loggingInLoadingMessage: state.users.loggingInLoadingMessage,
 });
 
 export default connect(
   mapStateToProps,
-  { signout }
+  { signout, markNotificationsAsRead }
 )(DisplayUser);
