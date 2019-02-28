3eda32e7e7116eefbaca82e3079f2ce427c72330
Huthman King <kinghuthman@gmail.com>
2019-02-28 14:47:26 -0500

    Commented out ReactQuill text editor - currently nonfunctional

--- frontend/src/components/forms/AddPostForm.js
+++ frontend/src/components/forms/AddPostForm.js
@@ -13,30 +13,65 @@ const AddPostFormWrapper = styled.form`
 	padding: 10px;
 `;
 
-
 class AddPostForm extends Component {
-	constructor(props) {
-		super(props)
-		this.state = { postBody: '' };
-		this.handleChange = this.handleChange.bind(this)
-		// this.onChange = this.onChange.bind(this)
+	state = { postBody: '' };
+	handleChange = e => this.setState({ [e.target.name]: e.target.value });
+	handleSubmit = e => {
+		e.preventDefault();
+		const { postBody } = this.state;
+		const { discussion_id, historyPush } = this.props;
+		return this.props.addPost(discussion_id, postBody, historyPush);
+	};
+	render() {
+		const { postBody } = this.state;
+		const { toggleAddPostForm } = this.props;
+		return(
+			<AddPostFormWrapper onSubmit = { this.handleSubmit }>
+				<h1>Add post form</h1>
+
+				<input
+					placeholder = 'Add post...'
+					name = 'postBody'
+					onChange = { this.handleChange }
+					value = { postBody }
+				/>
+
+				<button type = 'submit'>Submit</button>
+
+				<button
+					onClick = { toggleAddPostForm }
+					type = 'button' // prevents form submission
+				>Cancel</button>
+			</AddPostFormWrapper>
+		);
 	}
-	modules = {
-		toolbar: [
-		  [{ 'header': [1, 2, false] }],
-		  ['bold', 'italic', 'underline','strike', 'blockquote'],
-		  [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-		  ['link', 'image'],
-		  ['clean']
-		],
-	  }
+};
+
+//Text editor still being worked on
+
+// class AddPostForm extends Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = { postBody: '' };
+// 		this.handleChange = this.handleChange.bind(this)
+// 		// this.onChange = this.onChange.bind(this)
+// 	}
+// 	modules = {
+// 		toolbar: [
+// 		  [{ 'header': [1, 2, false] }],
+// 		  ['bold', 'italic', 'underline','strike', 'blockquote'],
+// 		  [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+// 		  ['link', 'image'],
+// 		  ['clean']
+// 		],
+// 	  }
 	 
-	 formats = [
-		'header', 'font', 'size',
-		'bold', 'italic', 'underline', 'strike', 'blockquote',
-		'list', 'bullet', 'indent',
-		'link', 'image', 'video'
-	  ]
+// 	 formats = [
+// 		'header', 'font', 'size',
+// 		'bold', 'italic', 'underline', 'strike', 'blockquote',
+// 		'list', 'bullet', 'indent',
+// 		'link', 'image', 'video'
+// 	  ]
 
 	//   AddPostForm.formats = [
 	// 	'header',
@@ -60,57 +95,57 @@ class AddPostForm extends Component {
 	// 	  matchVisual: false,
 	// 	}
 	//   }
-	  /* 
-	   * Quill editor formats
-	   * See https://quilljs.com/docs/formats/
-	   */
-	handleChange(html) {
-		this.setState({ postBody: (html) })
-	  }
+// 	  /* 
+// 	   * Quill editor formats
+// 	   * See https://quilljs.com/docs/formats/
+// 	   */
+// 	handleChange(html) {
+// 		this.setState({ postBody: (html) })
+// 	  }
 
 	  
 
-// onChange(postBody, delta, source, editor) {
-//   const text = editor.getContents(postBody);
-//   this.setState ({ postBody: text });
-// }
-	handleSubmit = e => {
-		e.preventDefault();
-		const { postBody } = this.state;
-		const { discussion_id, historyPush } = this.props;
-		return this.props.addPost(discussion_id, postBody, historyPush);
-	};
-	render() {
-		const { postBody } = this.state;
-		const { toggleAddPostForm } = this.props;
+// //onChange(postBody, delta, source, editor) {
+//  // const text = editor.getContents(postBody);
+//   //this.setState ({ postBody: text });
+// //}
+// 	handleSubmit = e => {
+// 		e.preventDefault();
+// 		const { postBody } = this.state;
+// 		const { discussion_id, historyPush } = this.props;
+// 		return this.props.addPost(discussion_id, postBody, historyPush);
+// 	};
+// 	render() {
+// 		const { postBody } = this.state;
+// 		const { toggleAddPostForm } = this.props;
 		
-		return(
-			<AddPostFormWrapper onSubmit = { this.handleSubmit }>
-				<h1>Add post form</h1>
+// 		return(
+// 			<AddPostFormWrapper onSubmit = { this.handleSubmit }>
+// 				<h1>Add post form</h1>
 
 				
-				{Parser(postBody)}
+// 				{Parser(postBody)}
 				
-				<ReactQuill
-					placeholder = 'Add post...'
-					name = 'postBody'
-					onChange = { this.handleChange }
-					value = {this.state.postBody}
-					modules={AddPostForm.modules}
-					formats={AddPostForm.formats}
-					dangerouslySetInnerHTML={{__html: this.state.postBody}}
-				/>
+// 				<ReactQuill
+// 					placeholder = 'Add post...'
+// 					name = 'postBody'
+// 					onChange = { this.handleChange }
+// 					value = {this.state.postBody}
+// 					modules={AddPostForm.modules}
+// 					formats={AddPostForm.formats}
+// 					dangerouslySetInnerHTML={{__html: this.state.postBody}}
+// 				/>
 			
-				<button type = 'submit'>Submit</button>
-				{/* <div dangerouslySetInnerHTML={{__html: this.state.postBody}}></div> */}
-				<button
-					onClick = { toggleAddPostForm }
-					type = 'button' // prevents form submission
-				>Cancel</button>
-			</AddPostFormWrapper>
-		);
-	}
-};
+// 				<button type = 'submit'>Submit</button>
+// 				{/* <div dangerouslySetInnerHTML={{__html: this.state.postBody}}></div> */}
+// 				<button
+// 					onClick = { toggleAddPostForm }
+// 					type = 'button' // prevents form submission
+// 				>Cancel</button>
+// 			</AddPostFormWrapper>
+// 		);
+// 	}
+// };
 
 
 export default connect(null, { addPost })(AddPostForm);
