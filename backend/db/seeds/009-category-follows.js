abbc12ce502158d258d79ac7ff845305f70a7b62
Carlos Vargas <cornielleandres@gmail.com>
2019-03-06 00:11:36 -0500

    Update seeds.

--- backend/db/seeds/009-category-follows.js
+++ backend/db/seeds/009-category-follows.js
@@ -4,17 +4,14 @@ const{
 
 const generateSeeds = () => {
   let arr = [];
-  for (let i = 1; i <= numOfCategories; i++) {
-    arr.push({
-      category_id: i,
-      user_id: i,
-    });
+  // every user profile created for the team will be following default categories
+  for (let i = 501; i <= 505; i++) {
+    for (let j = 1; j <= numOfCategories - 1; j++) { // - 1 here for testing purposes
+      arr.push({ category_id: j, user_id: i });
+    }
   }
-  // manually add 2 seeds to james fake profile
-  arr.push({ category_id: 1, user_id: 501 });
-  arr.push({ category_id: 2, user_id: 501 });
   return arr;
-}
+};
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
