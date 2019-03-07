928a40ed36336a5ad1d2573006ff195950d8547b
Huthman King <kinghuthman@gmail.com>
2019-03-06 19:22:05 -0500

    Settings

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -3,21 +3,15 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 import {phoneP, tabletP, } from '../globals/globals';
 
-// helpers
-import { isUserType } from '../helpers/index.js';
-
 // action creators
-import { getProfile, editUser, displayError } from '../store/actions/index.js';
+import { getProfile, editUser, } from '../store/actions/index.js';
 
 // components
 import {
   Avatar,
-  EditPasswordForm,
   EditAvatarForm,
   EditAvatarUrlForm,
-  UpdateEmailForm,
   DeleteAccountModal,
-  EditSignatureForm,
 } from './index.js';
 
 const SettingsWrapper = styled.div`
@@ -29,7 +23,6 @@ const SettingsWrapper = styled.div`
   box-shadow: ${props => props.theme.settingsBxShdw};
   border-radius: 30px;
   width: 90%;
-  margin-top: 100px;
   @media ${tabletP}{
     display: flex;
     flex-directon: column;      
@@ -41,7 +34,7 @@ const SettingsWrapper = styled.div`
   .fa-arrow-circle-left {
     font-size: 2rem;
     align-self: flex-start;
-    margin-left: 150px;
+    margin-left: 20px;
     cursor: pointer;
     @media ${tabletP}{
       margin-left: 40px;
@@ -52,87 +45,79 @@ const SettingsWrapper = styled.div`
   }
 `;
 
-const UsernameSettings = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    @media (max-width: 500px){
-      font-size: 12px;
-    }
-`;
 
 const UserProperties = styled.form`
+  width: 100%;
+  display:flex;
+  flex-wrap: wrap;
+  @media ${tabletP}{
+    flex-direction: column;
+    width: 60%;  
+    p {
+      padding-left: 5px;
+    } 
+  }
+  @media ${phoneP}{
     width: 100%;
-    display:flex;
-    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  .input-style {
+    margin-top: 5px;
+    margin-left: 1%;
+    border: 1px solid rgb(222,180,200, 0.2);
+    width: 180px;
+    height: 20px;
+    border-radius: 5px;
+    padding: 5px;
     @media ${tabletP}{
-      flex-direction: column;
-      width: 60%;  
-      p {
-        padding-left: 5px;
-      } 
+      width: 150%;      
     }
     @media ${phoneP}{
-      width: 100%;
-      justify-content: center;
-      align-items: center;
+      width: 95%;
+      margin-left: 0px;
     }
-    .input-style {
-      margin-top: 5px;
-      margin-left: 1%;
-      border: 1px solid rgb(222,180,200, 0.2);
-      width: 180px;
-      height: 20px;
-      border-radius: 5px;
-      padding: 5px;
-      @media ${tabletP}{
-        width: 150%;      
-      }
-      @media ${phoneP}{
-        width: 95%;
-        margin-left: 0px;
-      }
+  }
+  .delete-btn {
+    background-color: ${props => props.theme.settingsDeleteButtonBg};
+    color: ${props => props.theme.settingsDeleteButtonColor};
+    @media ${tabletP}{
+      width: 75%;      
     }
-    .delete-btn {
-      background-color: ${props => props.theme.settingsDeleteButtonBg};
-      color: ${props => props.theme.settingsDeleteButtonColor};
-      @media ${tabletP}{
-        width: 75%;      
-      }
-      @media ${phoneP}{
-        width: 90%;  
-        padding: 15px 0px 30px 0px;    
-      }
-      &:hover {
-        background-color: ${props => props.theme.settingsDeleteButtonBgHov};
-        cursor: pointer;
-      }
+    @media ${phoneP}{
+      width: 90%;  
+      padding: 15px 0px 30px 0px;    
     }
-    .save-settings {
-      background-color: ${props => props.theme.settingsButtonBgColor};
-      color: ${props => props.theme.settingsDeleteButtonColor};
-      @media ${tabletP}{
-        width: 75%;      
-      }
-      @media ${phoneP}{
-        width: 90%;  
-        padding: 15px 0px 30px 0px;    
-      }
-      &:hover {
-        background-color: ${props => props.theme.settingsButtonHov};
-        cursor: pointer;
-      }
+    &:hover {
+      background-color: ${props => props.theme.settingsDeleteButtonBgHov};
+      cursor: pointer;
     }
-    button {
-      width: 45%;
-      margin: 7px;
-      margin-top: 20px;
-      border-radius: 5px;
-      height: 30px;
-      font-size: 12px;
-      color: black;
-      justify-content: center;
-      align-items: center;
+  }
+  .save-settings {
+    background-color: ${props => props.theme.settingsButtonBgColor};
+    color: ${props => props.theme.settingsDeleteButtonColor};
+    @media ${tabletP}{
+      width: 75%;      
+    }
+    @media ${phoneP}{
+      width: 90%;  
+      padding: 15px 0px 30px 0px;    
+    }
+    &:hover {
+      background-color: ${props => props.theme.settingsButtonHov};
+      cursor: pointer;
+    }
+  }
+  button {
+    width: 45%;
+    margin: 7px;
+    margin-top: 20px;
+    border-radius: 5px;
+    height: 30px;
+    font-size: 12px;
+    color: black;
+    justify-content: center;
+    align-items: center;
       &:focus {
         outline: none;
       }
@@ -159,7 +144,7 @@ const UserSettings = styled.div`
 
 const ProfileSettings = styled.div`
   width: 30%;
-  margin-left: 100px;
+  margin-left: 0px;
   align-items: center;
   justify-content: center;
   .avatar-change{
@@ -184,7 +169,7 @@ const EmailAndAvatar = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-  justify-content: center;
+    justify-content: center;
     @media ${tabletP}{
       font-size: 16px;
     }
@@ -199,105 +184,10 @@ const AvatarPic = styled.div`
     display: flex;
     width: 100%;
     align-items: center;
-  justify-content: center;
+    justify-content: center;
     p {
       margin-right: 20px;
     }
-`;
-
-// const AuthOEditForms = styled.div`
-//     display: flex;
-//     justify-content: flex-end;
-//     width: 70%;
-//     padding-bottom: 10px;
-// `;
-
-const EditButtons = styled.div`
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    align-items: center;
-    justify-content: center;
-    width: 25%;
-    p {
-      margin-bottom: 7px;
-    }
-`;
-
-const EmailForm = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    .email {
-      width: 100%;
-      margin-bottom: 7px;
-      border-radius: 10px;
-      height: 50px;
-      font-size: 16px;
-      font-weight: bold;
-      &:hover {
-      background-color: ${props => props.theme.settingsButtonHov};
-      cursor: pointer;
-    }
-      &:focus {
-        outline: none;
-    }
-    @media (max-width: 520px){
-      font-size: 12px;
-    }
-    }
-`;
-
-const PasswordForm = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    .email {
-      width: 100%;
-      margin-bottom: 7px;
-      border-radius: 10px;
-      height: 50px;
-      font-size: 16px;
-      font-weight: bold;
-      &:hover {
-      background-color: ${props => props.theme.settingsButtonHov};
-      cursor: pointer;
-    }
-      &:focus {
-        outline: none;
-    }
-    @media (max-width: 520px){
-      font-size: 12px;
-    }
-    }
-`;
-const Buttons = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  button {
-    width: 100%;
-    margin: 7px;
-    border-radius: 10px;
-    height: 50px;
-    font-size: 16px;
-    font-weight: bold;
-    &:hover {
-      background-color: ${props => props.theme.settingsButtonHov};
-      cursor: pointer;
-    }
-    &:focus {
-      outline: none;
-    }
-    @media (max-width: 960px){
-      font-size: 14px;
-    }
-    @media (max-width: 520px){
-      font-size: 12px;
-    }
-  }
 `;
 
 const AvatarContainer = styled.div`
@@ -337,16 +227,6 @@ const EditAvatarMenu = styled.div`
   }
 `;
 
-const Signature = styled.div`
-  font-size: 20px;
-  width: 100%;
-  display: flex;
-  justify-content: flex-start;
-  flex-wrap: wrap;
-  p {
-    margin: 0px 0px 7px 0px;
-  }  
-`;
 const FirstName = styled.div `
   font-size: 14px;
   width: 50%;
@@ -424,68 +304,51 @@ class Settings extends Component {
   goBack = () => this.props.history.goBack()
   render() {
     const { showForm, showDeleteModal } = this.state;
-    const { profile, user_type, signature } = this.props;
-    const { username, email, avatar, isAuth0, email_confirm, password } = profile;
+    const { profile, } = this.props;
+    const { username, email, avatar, isAuth0, } = profile;
     const splitUsername = username.split(' ');
     return (
       <SettingsWrapper>
         <i onClick = {this.goBack} className = "fas fa-arrow-circle-left"/>
         {/* <UsernameSettings><h1>{username}'s Settings</h1></UsernameSettings> */}
         <UserSettings>
-        
           <ProfileSettings>
-          <EmailAndAvatar>
-          <AvatarPic>
-              <Avatar height='150px' width='150px' src={avatar} />
+            <EmailAndAvatar>
+            <AvatarPic>
+                <Avatar height='150px' width='150px' src={avatar} />
             </AvatarPic>
-           
-                <p className = 'avatar-change' onClick={() => this.toggleForm('avatar-btns')}>
-                  (change)
-                </p>
-            
-          </EmailAndAvatar>
-          
-          {/* {
-            signature !== null &&
-            <Signature>
-              <p>Signature:</p>
-              <p className = 'signature-text'>{ signature || 'none' }</p>
-            </Signature>
-          } */}
+                  <p className = 'avatar-change' onClick={() => this.toggleForm('avatar-btns')}>
+                    (change)
+                  </p>
+            </EmailAndAvatar>
           </ProfileSettings>
           <UserProperties onSubmit = {this.handleSubmit}>
             <FirstName><p> First Name <input className = 'input-style' name = 'firstName' placeholder = {splitUsername[0]} value = {this.state.firstName} onChange = {this.handleInputChange}/></p></FirstName>
             <FirstName><p> Last Name <input className = 'input-style' name = 'lastName' placeholder = {splitUsername[1]} value = {this.state.lastName} onChange = {this.handleInputChange} /></p></FirstName>
             <Email>
-              <p>Email {isAuth0 ? <p>{email}</p> : <input className = 'input-style' name = 'email' type = 'email' placeholder = {email} value = {this.state.email} onChange = {this.handleInputChange} /> }</p>
-              
-            
-              {/* <EmailForm>
-                {
-                  isAuth0 ?
-                  <p>You are Auth0. You cannot change your email.</p>
-                  :
-                  email ?
-                  <button className = 'change email' onClick = { () => this.toggleForm('email-form') }>Edit email</button>
-                  :
-                  <button className = 'set email' onClick = { () => this.toggleForm('email-form') }>Set email</button>
-                  
-                }
-                </EmailForm> */}
+              <p>Email {isAuth0 ? 
+                <p>{email}</p> 
+                  : 
+                  <input 
+                    className = 'input-style' 
+                    name = 'email' 
+                    type = 'email' 
+                    placeholder = {email} 
+                    value = {this.state.email} 
+                    onChange = {this.handleInputChange} 
+                    /> }
+                  </p>
             </Email>
               <Password>
                 <p>Old Password <input name = 'oldPassword' className = 'input-style' type = 'password' placeholder = 'enter old password' value = {this.state.oldPassword} onChange = {this.handleInputChange} /></p>
                 <p>New Password <input name = 'newPassword' className = 'input-style' type = 'password' placeholder = 'enter new password' value = {this.state.newPassword} onChange = {this.handleInputChange} /></p>
-                  {/* <PasswordForm>
-                    <button className = 'change email' onClick={() => this.toggleForm('password-form')}>
-                        Edit password
-                    </button>
-                  </PasswordForm> */}
               </Password>
-              <button className = 'save-settings' type = 'submit' >Save settings</button>
-              <button className = 'delete-btn' type = 'button' onClick={ this.toggleDeleteModal }>
-                Delete account
-              </button>
+                <button className = 'save-settings' type = 'submit' >
+                  Save settings
+                </button>
+                <button className = 'delete-btn' type = 'button' onClick={ this.toggleDeleteModal }>
+                  Delete account
+                </button>
               {showDeleteModal && (
                 <DeleteAccountModal toggleDeleteModal = { this.toggleDeleteModal } />
                 )}
@@ -516,35 +379,6 @@ class Settings extends Component {
             </AvatarContainer>
           )}
           </UserProperties>
-        {/* <AuthOEditForms>
-            <EditButtons>
-              
-              <Buttons>
-              {
-                // isUserType(user_type, ['silver_member', 'gold_member', 'admin']) &&
-                // <button onClick={() => this.toggleForm('signature-form')}>
-                //   Edit signature
-                // </button>
-              }
-              
-            </Buttons>
-          </EditButtons>
-          {showForm === 'password-form' && (
-            <EditPasswordForm toggleForm={this.toggleForm} />
-          )}
-          
-          {showForm === 'signature-form' && (
-            <EditSignatureForm signature = { signature } toggleForm={this.toggleForm} />
-          )}
-          
-          {showForm === 'email-form' && (
-            <UpdateEmailForm
-              toggleForm={this.toggleForm}
-              history = { this.props.history }
-            />
-          )}
-          
-        </AuthOEditForms> */}
         </UserSettings>
       </SettingsWrapper>
     );
