a59b042c2ea8dbf7b7641126f33ab43fe61bbb96
jamespagedev <jamespagedev@outlook.com>
2019-03-11 06:48:10 -0700

    must be bronze or higher to see Add Post button

--- frontend/src/components/DiscussionsByFollowedCats.js
+++ frontend/src/components/DiscussionsByFollowedCats.js
@@ -1,6 +1,6 @@
-import React, { Component }	from 'react';
-import { connect }			from 'react-redux';
-import styled				from 'styled-components';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 // action creators
 import { getAllDiscussionsByFollowedCategories, handleDiscussionVote } from '../store/actions/index.js';
@@ -9,7 +9,7 @@ import { getAllDiscussionsByFollowedCategories, handleDiscussionVote } from '../
 import { DiscussionByFollowedCats, AddDiscussionForm } from './index.js';
 
 // globals
-import { phoneP, tabletP } from '../globals/globals.js';
+import { phoneP, tabletP, accountUserTypes, addPostPermStartIndex } from '../globals/globals.js';
 
 const DiscussionsWrapper = styled.div`
 	display: flex;
@@ -33,7 +33,7 @@ const DiscussionsWrapper = styled.div`
 		align-items: center;
 		width: 95%;
 
-		@media ${ tabletP } {
+		@media ${ tabletP} {
 			width: 100%;
 		}
 	}
@@ -51,7 +51,7 @@ const DiscussionHeader = styled.div`
 		font-size: 36px;
 		flex-grow: 2;
 
-		@media ${ tabletP } {
+		@media ${ tabletP} {
 			flex-grow: 0;
 		}
 	}
@@ -80,7 +80,7 @@ const DiscussionHeader = styled.div`
 		background-color: #418DCF;
 		color: white;
 
-		@media ${ phoneP } {
+		@media ${ phoneP} {
 			width: 100%;
 			margin-left: 0;
 		}
@@ -101,99 +101,101 @@ const mostViews = 'most views';
 const mostComments = 'most comments';
 
 class AllDiscussionsByFollowedCats extends Component {
-	state = { filter: newest, followedDiscussions: [], showAddDiscussionForm: false };
-	toggleAddDiscussionForm = () => this.setState({
-		showAddDiscussionForm: !this.state.showAddDiscussionForm,
-	});
-	handleFilterChange = () => {
-		const { filter, followedDiscussions } = this.state;
-		let newFollowedDiscussions;
-		switch(filter) {
-			case newest: {
-				newFollowedDiscussions = followedDiscussions.sort((a, b) => b.created_at - a.created_at);
-				return this.setState({ followedDiscussions: newFollowedDiscussions });
-			}
-			case oldest: {
-				newFollowedDiscussions = followedDiscussions.sort((a, b) => a.created_at - b.created_at);
-				return this.setState({ followedDiscussions: newFollowedDiscussions });
-			}
-			case mostUpvotes: {
-				newFollowedDiscussions = followedDiscussions.sort((a, b) => b.upvotes - a.upvotes);
-				return this.setState({ followedDiscussions: newFollowedDiscussions });
-			}
-			case mostViews: {
-				newFollowedDiscussions = followedDiscussions.sort((a, b) => b.views - a.views);
-				return this.setState({ followedDiscussions: newFollowedDiscussions });
-			}
-			case mostComments: {
-				newFollowedDiscussions = followedDiscussions.sort((a, b) => b.post_count - a.post_count);
-				return this.setState({ followedDiscussions: newFollowedDiscussions });
-			}
-			default:
-				return;
-		}
-	};
-	handleSelectChange = e => this.setState({
-		[e.target.name]: e.target.value,
-	}, () => this.handleFilterChange());
-	getDiscussions = () => this.props.getAllDiscussionsByFollowedCategories()
-		.then(() => this.setState({ followedDiscussions: this.props.followedDiscussions }));
-	voteOnDiscussion = (id, type) => this.props.handleDiscussionVote(id, type)
-		.then(() => this.getDiscussions())
-		.then(() => this.handleFilterChange());
-	componentDidMount = () => this.getDiscussions();
-	render() {
-		const { followedDiscussions, showAddDiscussionForm } = this.state;
-		const { history, match } = this.props;
-		return(
-			<DiscussionsWrapper>
-				<DiscussionHeader>
-					<h2 className = 'all-posts'>All Posts</h2>
-					<div className = 'filter-wrapper'>
-						<i className = 'fab fa-mix' />
-						<span>Filter by</span>
-						<select
-							className = 'filter'
-							onChange = { this.handleSelectChange }
-							name = 'filter'
-						>
-							<option value = { newest }>{ newest }</option>
-							<option value = { oldest }>{ oldest }</option>
-							<option value = { mostUpvotes }>{ mostUpvotes }</option>
-							<option value = { mostViews }>{ mostViews }</option>
-							<option value = { mostComments }>{ mostComments }</option>
-						</select>
-					</div>
-					<button onClick = { this.toggleAddDiscussionForm } className = 'add-post-btn'>
-						<i className = 'fas fa-plus-circle' />&nbsp;Add Post
-					</button>
-				</DiscussionHeader>
-				<hr/>
-				<div className = 'content'>
-					{ followedDiscussions.map((discussion, i) =>
-						<DiscussionByFollowedCats
-							key = { i }
-							discussion = { discussion }
-							history = { history }
-							voteOnDiscussion = { this.voteOnDiscussion }
-						/>)
-					}
-				</div>
-				{
-					showAddDiscussionForm &&
-					<AddDiscussionForm
-						toggleAddDiscussionForm = { this.toggleAddDiscussionForm }
-						getDiscussions = { this.getDiscussions }
-						category_id = { match.params.category_id }
-					/>
-				}
-			</DiscussionsWrapper>
-		);
-	}
+  state = { filter: newest, followedDiscussions: [], showAddDiscussionForm: false };
+  toggleAddDiscussionForm = () => this.setState({
+    showAddDiscussionForm: !this.state.showAddDiscussionForm,
+  });
+  handleFilterChange = () => {
+    const { filter, followedDiscussions } = this.state;
+    let newFollowedDiscussions;
+    switch (filter) {
+      case newest: {
+        newFollowedDiscussions = followedDiscussions.sort((a, b) => b.created_at - a.created_at);
+        return this.setState({ followedDiscussions: newFollowedDiscussions });
+      }
+      case oldest: {
+        newFollowedDiscussions = followedDiscussions.sort((a, b) => a.created_at - b.created_at);
+        return this.setState({ followedDiscussions: newFollowedDiscussions });
+      }
+      case mostUpvotes: {
+        newFollowedDiscussions = followedDiscussions.sort((a, b) => b.upvotes - a.upvotes);
+        return this.setState({ followedDiscussions: newFollowedDiscussions });
+      }
+      case mostViews: {
+        newFollowedDiscussions = followedDiscussions.sort((a, b) => b.views - a.views);
+        return this.setState({ followedDiscussions: newFollowedDiscussions });
+      }
+      case mostComments: {
+        newFollowedDiscussions = followedDiscussions.sort((a, b) => b.post_count - a.post_count);
+        return this.setState({ followedDiscussions: newFollowedDiscussions });
+      }
+      default:
+        return;
+    }
+  };
+  handleSelectChange = e => this.setState({
+    [e.target.name]: e.target.value,
+  }, () => this.handleFilterChange());
+  getDiscussions = () => this.props.getAllDiscussionsByFollowedCategories()
+    .then(() => this.setState({ followedDiscussions: this.props.followedDiscussions }));
+  voteOnDiscussion = (id, type) => this.props.handleDiscussionVote(id, type)
+    .then(() => this.getDiscussions())
+    .then(() => this.handleFilterChange());
+  componentDidMount = () => this.getDiscussions();
+  render() {
+    const { followedDiscussions, showAddDiscussionForm } = this.state;
+    const { history, match, user_type } = this.props;
+    return (
+      <DiscussionsWrapper>
+        <DiscussionHeader>
+          <h2 className='all-posts'>All Posts</h2>
+          <div className='filter-wrapper'>
+            <i className='fab fa-mix' />
+            <span>Filter by</span>
+            <select
+              className='filter'
+              onChange={this.handleSelectChange}
+              name='filter'
+            >
+              <option value={newest}>{newest}</option>
+              <option value={oldest}>{oldest}</option>
+              <option value={mostUpvotes}>{mostUpvotes}</option>
+              <option value={mostViews}>{mostViews}</option>
+              <option value={mostComments}>{mostComments}</option>
+            </select>
+          </div>
+          {(accountUserTypes.indexOf(user_type) >= addPostPermStartIndex) &&
+            <button onClick={this.toggleAddDiscussionForm} className='add-post-btn'>
+              <i className='fas fa-plus-circle' />&nbsp;Add Post
+          </button>}
+        </DiscussionHeader>
+        <hr />
+        <div className='content'>
+          {followedDiscussions.map((discussion, i) =>
+            <DiscussionByFollowedCats
+              key={i}
+              discussion={discussion}
+              history={history}
+              voteOnDiscussion={this.voteOnDiscussion}
+            />)
+          }
+        </div>
+        {
+          showAddDiscussionForm &&
+          <AddDiscussionForm
+            toggleAddDiscussionForm={this.toggleAddDiscussionForm}
+            getDiscussions={this.getDiscussions}
+            category_id={match.params.category_id}
+          />
+        }
+      </DiscussionsWrapper>
+    );
+  }
 };
 
 const mapStateToProps = state => ({
-	followedDiscussions: state.discussions.followedDiscussions,
+  user_type: state.users.user_type,
+  followedDiscussions: state.discussions.followedDiscussions,
 });
 
 export default connect(mapStateToProps, { getAllDiscussionsByFollowedCategories, handleDiscussionVote })(AllDiscussionsByFollowedCats);
