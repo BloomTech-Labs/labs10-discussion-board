88bfa44ae07c78df8ef509224d22c0a1eecfc7ea
Carlos Vargas <cornielleandres@gmail.com>
2019-03-13 21:47:54 -0400

    Delete commented out code.

--- frontend/src/components/RegisterDropdown.js
+++ frontend/src/components/RegisterDropdown.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 // action creators
@@ -11,13 +10,6 @@ import { ToolTip } from './index.js';
 
 //globals
 import { phoneL } from '../globals/globals.js';
-
-// globals
-import {
-//   auth0Domain,
-//   auth0ClientID,
-//   auth0RedirectUri
-} from '../globals/globals.js';
 
 /***************************************************************************************************
  ********************************************** Styles *********************************************
