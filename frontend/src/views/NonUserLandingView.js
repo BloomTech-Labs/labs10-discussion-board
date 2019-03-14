46d0791f655ebcf849c33c260a50350e68989485
Carlos Vargas <cornielleandres@gmail.com>
2019-03-13 20:03:34 -0400

    Update landing page.

--- frontend/src/views/NonUserLandingView.js
+++ frontend/src/views/NonUserLandingView.js
@@ -1,93 +1,115 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import styled from 'styled-components';
-import { tabletP } from '../globals/globals';
-import { socrates} from '../assets/index.js';
+import { connect } from 'react-redux';
+import { socrates } from '../assets/index.js';
 
 const Woah = styled.div `
   width: 100%;
-  height: 100%;
+  height: 67vh;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   flex-wrap: wrap;
-  flex-direction: row;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-end;
   background-image: url(${socrates});
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: left;
   background-size: contain;
 
-.blurb{
-    width: 300px;
-    font-size: 1.4rem;
-    position: absolute;
-    height: 100px;
-    top: 180px;
-    right: 100px;
-}
+  @media (max-width: 700px) {
+    background-image: none;
+    background-color: white;
+  }
+
+  ${ ({ loggedIn }) => loggedIn && '@media (max-width: 980px) {background-image: none;background-color: white;}' }
+  }}
 `;
 
 const LandingText = styled.div `
-    height: 100%
-    width: 40%
-    `
-const VideoPlayer = styled.div `
-    width: 50%;
-    padding-top: 40%
-    position: relative;
-    .vid-player{
-         width: 100%;
-         height: 100%;
-         position: absolute;
-         top: 0;
-      @media ${tabletP} {
-          width: 50%;
-        }
-    }
-    @media (max-width: 1350px){
-        padding-top: 26%;
-        width: 50%;
-    }
+  height: 40%;
+  width: 50%;
+  margin-right: 30px;
+
+  @media (max-width: 700px) {
+    width: 100%;
+    background-color: white;
+  }
+
+  ${ ({ loggedIn }) => loggedIn && '@media (max-width: 980px) {width: 100%;background-color: white;}' }
+
+  .blurb{
+    font-size: 1.4rem;
+    font-weight: bold;
+  }
 `;
 
-const TopDiscussionsImage = styled.img`
-   src: url(${socrates});
-    display: flex;
-    height: 100%;
-   width: 100%;
- `;
+const VideoPlayer = styled.div `
+  position: relative;
 
-const Vidception = styled.div `
-  width: 60%;
-  height: 100%;
-  padding: 130px;
-  padding-left: 0px;
-  `;
+  @media (max-width: 700px) {
+    position: absolute;
+    top: 40%;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  ${ ({ loggedIn }) => loggedIn && '@media (max-width: 980px) {position: absolute;top: 40%;left: 0;right: 0;margin-left: auto;margin-right: auto;}' }
+
+  .vid-player{
+    width: 1400px;
+    height: 212px;
+    position: absolute;
+    top: 0;
+    right: 30px;
+
+    @media (max-width: 700px) {
+      left: 0;
+      right: 0;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    ${ ({ loggedIn }) => loggedIn && '@media (max-width: 980px) {left: 0;right: 0;margin-left: auto;margin-right: auto;}' }
+  }
+`;
+
+// const Vidception = styled.div `
+//   width: 60%;
+//   height: 100%;
+//   padding: 130px;
+//   padding-left: 0px;
+//   `;
 
 
 class NonUserLandingView extends Component {
   render () {
     return(
 
-        <Woah>
-          <LandingText>
+        <Woah loggedIn = { this.props.user_id !== 0 }>
+          <LandingText loggedIn = { this.props.user_id !== 0 }>
             <p className = 'blurb'>
               Symposium is a place where like-minded people can come together to share and discuss ideas.
             </p>
           </LandingText>
-          <Vidception>
-            <VideoPlayer>
-              <ReactPlayer 
-              className = 'vid-player' 
-              url='https://youtu.be/T-Dji780Ro0' 
-              width = '100%' height = '100%' 
-              controls = {true}/>
-            </VideoPlayer>
-          </Vidception>
-        
-        </Woah>
-         )
-  }
-}
 
-export default NonUserLandingView;
+          <VideoPlayer loggedIn = { this.props.user_id !== 0 }>
+          <ReactPlayer 
+            className = 'vid-player' 
+            url='https://youtu.be/T-Dji780Ro0' 
+            width = '320px' height = '180px' 
+            controls = {true}
+          />
+          </VideoPlayer>
+        </Woah>
+        )
+  }
+};
+
+const mapStateToProps = state => ({
+  user_id: state.users.user_id,
+});
+
+export default connect(mapStateToProps, {})(NonUserLandingView);
